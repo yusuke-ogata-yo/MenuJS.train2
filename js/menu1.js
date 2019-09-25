bdfd444775d5ae21c61dd686729beab50575dcd2
Yusuke Ogata <yusuke.ogata.yo@gmail.com>
2019-09-26 06:41:09 +0900

    copy the original menu1.html, css, js

---
+++ js/menu1.js
@@ -0,0 +1,10 @@
+'use strict';
+
+{
+  let show = document.getElementById('show');
+  let main = document.getElementById('main');
+
+  show.addEventListener('click', function() {
+    main.className = main.className === 'menu-open' ? '': 'menu-open';
+  });
+};
